e2955064f6887424e1116a80ae5bd5b6470dac7b
tangshiya <tsy17321289030@163.com>
2020-05-15 16:38:31 +0800

    useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
+/**
+ * useState返回值有两个
+ * 第一个是当前state
+ * 第二个修改state的函数
+ * 0表示count初始值
+ */
 function App() {
+  const [ count, setCount ] = useState(0)
   return (
     <div className="App">
-      hello world
+      <p>You clicked { count } times</p>
+      <button onClick={() => setCount(count + 1)}>Clicked me</button>
     </div>
   )
 }
